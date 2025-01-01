fd7d55acd81b1a4e2dfd93679c399ceae43e7c10
Hong-Nhien <nhientth1415@ut.edu.vn>
2025-01-02 03:12:48 +0700

    add

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+     path('cart/', views.cart, name='cart'),
+      path('checkout/', views.checkout, name='checkout'),
    
 
 ]

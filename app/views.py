d3102a042bb6c42424b131d54a9ff40d0aefbdae
Hong-Nhien <nhientth1415@ut.edu.vn>
2025-01-05 13:52:41 +0700

    add

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
@@ -9,13 +10,13 @@ def home(request):
     return render(request, 'app/home.html', context)
 def cart(request):
     if request.user.is_authenticated:
-        customer= request.user.customer
-        order, created = Order.objects.get_or_create(customer =customer,complete=False) 
-
-        items = order.oderitems_set.all()
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(Customer=customer, complete=False)
+        items = order.orderitem_set.all()
     else:
-        items = []    
-    context={'items':items,'order':order}
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, 'app/cart.html', context)
 def checkout(request):
     context={}

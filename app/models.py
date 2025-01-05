e8637046341d29223cbe516a79849893025b46c7
Hong-Nhien <nhientth1415@ut.edu.vn>
2025-01-05 10:39:13 +0700

    add

--- app/models.py
+++ app/models.py
@@ -16,10 +16,19 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
 
     def __str__(self):
         return self.name
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 class Oder(models.Model):
     Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=False)
     date_oder = models.DateTimeField(auto_now_add=True)

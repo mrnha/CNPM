2c39d50ba0b1b09dba55d90681099d6ee7fad488
mrnha <vannha.12345k2@gmail.com>
2025-01-06 15:09:38 +0700

    add

--- app/views.py
+++ app/views.py
@@ -15,9 +15,18 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'order.get_cart_items':0,'order.get_cart_total':0}
+        
     context = {'items': items, 'order': order}
     return render(request, 'app/cart.html', context)
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(Customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'order.get_cart_items':0,'order.get_cart_total':0}
+        
+    context = {'items': items, 'order': order}
     return render(request, 'app/checkout.html', context)

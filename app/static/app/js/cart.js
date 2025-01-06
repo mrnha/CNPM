d401d9cd86d1f065b67b7f005aa6d2b2e70ef2b6
mrnha <vannha.12345k2@gmail.com>
2025-01-06 17:07:49 +0700

    add

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -4,7 +4,14 @@ for(var i=0;i < updateBtns.length;i++){
     updateBtns[i].addEventListener('click', function() {
         var productId = this.dataset.product
         var action = this.dataset.action
-        connsole.console.log('product',productId,'action',action);
+        console.log('product',productId,'action',action);
+        console.log('user:', user)
+        if (user==="AnoymousUser"){
+            console.log('user not logged in')
+        }else {
+            console.log('user logged in, success add')
+
+            }
         
 
     })
